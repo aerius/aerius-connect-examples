488ca2db8a7e3fbe2f5de9641830b405c8eb9913
Hilbrand Bouwkamp <hilbrand@h72.nl>
2015-09-18 11:12:12 +0200

    Fix tabs and added documentation.
    For calculationAndEmail, e-mail has become a command line argument.

--- python/v2/AERIUS_calculateAndEmail.py
+++ python/v2/AERIUS_calculateAndEmail.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Dit script is als voorbeeld om het gebruik van AERIUS connect te demonstreren.
+
+Dit voorbeeld script vertuurt de invoer naar AERIUS connect om berekend te worden.
+Indien de invoer niet valide is wordt dit teruggegeven. Als de invoer wel goed was,
+wordt de berekening gestart en het resultaat verstuurt naar het opgegeven e-mailadres.
+"""
 import json
 import time
 import os
@@ -7,115 +14,98 @@ import sys
 import getopt
 import websocket
 
-AERIUS_SERVER = "connect.aerius.nl"
+AERIUS_SERVER = "ws://connect.aerius.nl"
 debug = False
 
-"""
-Open the file, create json object send this json to AERIUS connect endpoint
-Process the result test on successful and write the result to a result file
-"""
-def gml_calculateAndEmail(inputfile,comparefile):
-    compare = ""
-    f = open(inputfile,'r')
-    data = f.read()
-    f.close
-    if comparefile != "":
-        f = open(comparefile,'r')
-        compare = f.read()
-        f.close
-    # create json text object
-    json_text = """
-    {
-        "jsonrpc":"2.0",
-        "id":0,
-        "method":"calculation.calculateAndEmail",
-        "params":{
-			"email":"aeriusmail+connect@gmail.com",
+def calculateAndEmail(inputfile, email):
+	compare = ""
+	f = open(inputfile,'r')
+	data = f.read()
+	f.close
+	# create json text object
+	json_text = """
+	{
+		"jsonrpc":"2.0",
+		"id":0,
+		"method":"calculation.calculateAndEmail",
+		"params":{
+			"email":"",
 			"options":{
 				"calculationType":"NBWET",
 				"year":2015,
-				"substances":["NOX","NH3"],
-				"maximumRange":10000,
-				"tempProject":false,
-				"tempProjectYears":1
+				"substances":["NOX","NH3"]
 			},
 			"data":[{
 				"dataType":"GML",
 				"contentType":"TEXT",
 				"data":""}]
-        }
-    }
-    """
-    # replace paramaters
-    print(json_text)
-    json_data = json.loads(json_text)
-    json_data["id"] = int(time.time() * 1000) #create unique id
-    json_data["params"]["data"][0]["data"] = data
-    if compare != "":
-        row = json_data["params"]["data"][0]
-        json_data["params"]["data"].append(row)
-        json_data["params"]["data"][1]["data"] = compare
-	
-    #print(json.dumps(json_data))
-	
-    try:
-        ws = websocket.create_connection("ws://" + AERIUS_SERVER + "/connect/2/services")
-    except Exception as e:
-        print ("Unexpected connection error:", e)
-        return
+		}
+	}
+	"""
+	# replace paramaters
+	json_data = json.loads(json_text)
+	json_data["id"] = int(time.time() * 1000) #create unique id
+	json_data["params"]["data"][0]["data"] = data
+	json_data["params"]["email"] = email
+	print(json_text)
 
-    try:
-        #sending data
-        ws.send(json.dumps(json_data))
-        result = ws.recv()
-        ws.close()
-        if debug:
-            print ("(DEBUG json data send:)")
-            print (json.dumps(json_data))
-            print ("(DEBUG json data recieved:)")
-            print (result)
-        # write result part
-        if result.find("successful") > -1:
-            json_output = json.loads(result)
-            if (result.find("errors") > -1):
-                for error in json_output["result"]["errors"]:
-                    print (error["errorCode"] + " - " + error["description"])
-            else:
-                print ("gml calculation added to the qui")
-        else: 
-            # we have JSON-RPC error
-            json_output = json.loads(result)
-            error = json_output["error"]
-            print ("error: " + str(error["code"]) + " - " + error["message"])
+	#print(json.dumps(json_data))
 
-    except Exception as e:
-        print ("Unexpected result error:", e)
-        
-    finally:
-        ws.close()
-        
-    return
-    
+	try:
+		ws = websocket.create_connection(AERIUS_SERVER + "/connect/2/services")
+	except Exception as e:
+		print ("Unexpected connection error:", e)
+		return
+
+	try:
+		#sending data
+		ws.send(json.dumps(json_data))
+		result = ws.recv()
+		ws.close()
+		if debug:
+			print ("(DEBUG json data send:)")
+			print (json.dumps(json_data))
+			print ("(DEBUG json data recieved:)")
+			print (result)
+		# write result part
+		if result.find("successful") > -1:
+			json_output = json.loads(result)
+			if (result.find("errors") > -1):
+				for error in json_output["result"]["errors"]:
+					print (error["errorCode"] + " - " + error["description"])
+			else:
+				print ("gml calculation added to the queue")
+		else: 
+			# we have JSON-RPC error
+			json_output = json.loads(result)
+			error = json_output["error"]
+			print ("error: " + str(error["code"]) + " - " + error["message"])
+
+	except Exception as e:
+		print ("Unexpected result error:", e)
+
+	finally:
+		ws.close()
+
+	return
+
 def main(argv):
-    comparefile = ""
-    opts, args = getopt.getopt(argv,"hdi:c:",["ifile=","cfile="])
-    for opt, arg in opts:
-        if opt == '-h':
-            print('AERIUS_calculateAndEmail.py [-d] -i <gml file current situation> -c <gml file compare with new situation')
-            sys.exit()
-        elif opt == '-d':
-            global debug
-            debug = True
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-c", "--cfile"):
-            comparefile = arg
+	opts, args = getopt.getopt(argv,"hdi:e:",["ifile=","eemail="])
+	for opt, arg in opts:
+		if opt == '-h':
+			print('AERIUS_calculateAndEmail.py [-d] -i <gml file> -e <email address>')
+			sys.exit()
+		elif opt == '-d':
+			global debug
+			debug = True
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-e", "--eemail"):
+			email = arg
 
-    print("reading ", inputfile)
-    if comparefile != "":
-        print("reading", comparefile)
-		
-    gml_calculateAndEmail(inputfile,comparefile)
+	print("reading ", inputfile)
+
+	calculateAndEmail(inputfile,email)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+	main(sys.argv[1:])
--- python/v2/AERIUS_convert2GML.py
+++ python/v2/AERIUS_convert2GML.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Dit script is als voorbeeld om het gebruik van AERIUS connect te demonstreren.
+
+Dit voorbeeld script verstuurt de GML naar AERIUS connect en geeft een naar
+de laatste versie van IMAER GML geconverteerd document terug of indien er
+fouten waren deze terug gegeven.
+"""
 import json
 import time
 import os
@@ -7,91 +14,87 @@ import sys
 import getopt
 import websocket
 
-AERIUS_SERVER = "connect.aerius.nl"
+AERIUS_SERVER = "ws://connect.aerius.nl"
 debug = False
 
-"""
-Open the file, create json object send this json to AERIUS connect endpoint
-Process the result test on successful and write the result to a result file
-"""
-def gml_convert2GML(inputfile,outputfile):
-    f = open(inputfile,'r')
-    data = f.read()
-    f.close
-    # create json text object
-    json_text = """
-    {
-        "jsonrpc":"2.0",
-        "id":0,
-        "method":"conversion.convert2GML",
-        "params":{
-            "dataType":"GML",
-            "contentType":"TEXT",
-            "data":""
-        }
-    }
-    """
-    # replace paramaters
-    json_data = json.loads(json_text)
-    json_data["id"] = int(time.time() * 1000) #create unique id
-    json_data["params"]["data"] = data
+def convert2GML(inputfile,outputfile):
+	f = open(inputfile,'r')
+	data = f.read()
+	f.close
+	# create json text object
+	json_text = """
+	{
+		"jsonrpc":"2.0",
+		"id":0,
+		"method":"conversion.convert2GML",
+		"params":{
+			"dataType":"GML",
+			"contentType":"TEXT",
+			"data":""
+		}
+	}
+	"""
+	# replace paramaters
+	json_data = json.loads(json_text)
+	json_data["id"] = int(time.time() * 1000) #create unique id
+	json_data["params"]["data"] = data
 
-    try:
-        ws = websocket.create_connection("ws://" + AERIUS_SERVER + "/connect/2/services")
-    except Exception as e:
-        print ("Unexpected connection error:", e)
-        return
+	try:
+		ws = websocket.create_connection(AERIUS_SERVER + "/connect/2/services")
+	except Exception as e:
+		print ("Unexpected connection error:", e)
+		return
 
-    try:
-        #sending data
-        ws.send(json.dumps(json_data))
-        result = ws.recv()
-        ws.close()
-        if debug:
-            print ("(DEBUG json data send:)")
-            print (json.dumps(json_data))
-            print ("(DEBUG json data recieved:)")
-            print (result)
-        # write result part
-        if result.find("successful") > -1:
-            json_output = json.loads(result)
-            if (result.find("errors") > -1):
-                for error in json_output["result"]["errors"]:
-                    print (error["errorCode"] + " - " + error["description"])
-            else:
-                print ("writing converted content to ", outputfile)
-                fileOut = open(outputfile, "w+")
-                fileOut.write(json_output["result"]["dataObject"]["data"])
-        else: 
-            # we have JSON-RPC error
-            json_output = json.loads(result)
-            error = json_output["error"]
-            print ("error: " + str(error["code"]) + " - " + error["message"])
+	try:
+		#sending data
+		ws.send(json.dumps(json_data))
+		result = ws.recv()
+		ws.close()
+		if debug:
+			print ("(DEBUG json data send:)")
+			print (json.dumps(json_data))
+			print ("(DEBUG json data recieved:)")
+			print (result)
+		# write result part
+		if result.find("successful") > -1:
+			json_output = json.loads(result)
+			if (result.find("errors") > -1):
+				for error in json_output["result"]["errors"]:
+					print (error["errorCode"] + " - " + error["description"])
+			else:
+				print ("writing converted content to ", outputfile)
+				fileOut = open(outputfile, "w+")
+				fileOut.write(json_output["result"]["dataObject"]["data"])
+		else: 
+			# we have JSON-RPC error
+			json_output = json.loads(result)
+			error = json_output["error"]
+			print ("error: " + str(error["code"]) + " - " + error["message"])
 
-    except Exception as e:
-        print ("Unexpected result error:", e)
-        
-    finally:
-        ws.close()
-        
-    return
-    
+	except Exception as e:
+		print ("Unexpected result error:", e)
+		
+	finally:
+		ws.close()
+		
+	return
+	
 def main(argv):
-    opts, args = getopt.getopt(argv,"hdi:o:",["ifile=","ofile="])
-    for opt, arg in opts:
-        if opt == '-h':
-            print('AERIUS_convert2GML.py [-d] -i <gml file> -o <gml file>')
-            sys.exit()
-        elif opt == '-d':
-            global debug
-            debug = True
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
+	opts, args = getopt.getopt(argv,"hdi:o:",["ifile=","ofile="])
+	for opt, arg in opts:
+		if opt == '-h':
+			print('AERIUS_convert2GML.py [-d] -i <gml file> -o <gml file>')
+			sys.exit()
+		elif opt == '-d':
+			global debug
+			debug = True
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
 
-    print("reading ", inputfile)
-    gml_convert2GML(inputfile, outputfile)
+	print("reading ", inputfile)
+	convert2GML(inputfile, outputfile)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
